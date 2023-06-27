6f357efea15305d2c6ba13d089da4f39fb2dc298
Jeff Ellingham <jeffinghamm@gmail.com>
2023-06-27 02:55:47 -0400

    Initial commit with fully functional code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TODO-list-app
- Simple To-Do list app I created to learn jQuery
+ Simple To-Do list app I created to learn jQuery during my intro to JavaScript course in CareerFoundry's full-stack web development bootcamp. It was a quick exercise we did in one session to get familiar with jQuery while building a commonly suggested project.
+
+ I do hope to come back to this project someday to enhance its functionality and interface as I learn new things, and maybe even make it into a personalized app for my phone.
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+function newItem(){
+
+    //Adding a new item to the list:
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert('Type something, silly!');
+    } else {
+        $('#list').append(li);
+    }
+
+    // Crossing out an item
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+
+    li.on('click', crossOut);
+
+    // Adding the "X" delete button
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton)
+    
+    crossOutButton.on('click', deleteListItem);
+   
+    //Adding delete function for crossOutButton
+    function deleteListItem() {
+        li.addClass('delete');
+    }
+
+    // So you can reorder the list
+    $('#list').sortable();
+
+    // Pressing enter will add text
+    // $('#button').on('keydown', (e) => {
+    //     if(e.which === 13 && inputValue != '') {
+    //         $('#list').append(li);
+    //     }
+    // });
+}
